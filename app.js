0984e10897fe64b5d94aaf25cad588091e50dd2e
Jairo Iglesias (SPARK) <jairo.iglesias@sparkconsultoria.com>
2019-11-10 13:12:52 -0300

    Update README

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var load = require('express-load');
 var session = require('express-session');
+var cors = require('cors')
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'jade');
+
+app.options('*', cors()) // include before other routes 
+app.use(cors())
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
